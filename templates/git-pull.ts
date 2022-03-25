15c1c778f781461061576ea93be03d501e2efd41
Janik Schumacher <rndmb0t@gmail.com>
2022-03-25 13:15:46 +0100

    added test info for pull

--- templates/git-pull.ts
+++ templates/git-pull.ts
@@ -2,8 +2,16 @@ import { executeCmd } from "../execute-cmd.js";
 
 const gitPullInFolder = async (folder: string) => {
   const a = folder.split("/");
+  let test = false;
+  if (folder.includes("test")) {
+    test = true;
+  }
   const projectName = a[a.length - 1];
-  return executeCmd("git pull", `[${projectName}] `, folder);
+  return executeCmd(
+    "git pull",
+    `[${projectName}${test ? " (test)" : ""}] `,
+    folder
+  );
 };
 
 const projects = ["%%PROJECTS%%"];
